ee81f33529e27ee9b8ca466c9ea07cd935aa5a20
huishan3 <d108.815134@yuda.tyc.edu.tw>
2026-08-14 23:32:59 +0800

    Add shared Firebase project configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCMO_9eufgKuEoJ5Kob5W4SuDikaX-Drqg',
+  authDomain: 'product-display-system.firebaseapp.com',
+  projectId: 'product-display-system',
+  storageBucket: 'product-display-system.firebasestorage.app',
+  messagingSenderId: '253942464889',
+  appId: '1:253942464889:web:1cd581c08faf962ea4efa8',
+  measurementId: 'G-NTG56CZC9N'
+};
+
+export const melodyAppId = '1:253942464889:web:1cd581c08faf962ea4efa8';
